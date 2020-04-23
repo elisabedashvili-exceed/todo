4f53e0cbc6058b2f5bc75e8b96efec6e0b32af92
Vakhtang Elisabedashvili <vaxo_nba@yahoo.com>
2020-04-23 14:44:35 +0400

    routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,35 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// send array of todoitems
+router.post('/', (req, res, next) => {
+  res.send([]);
+});
+
+// create todoitem
+router.post('/', (req, res, next) => {
+  res.send([]);
+});
+
+// edit todoitem
+router.post('/', (req, res, next) => {
+  res.send([]);
+});
+
+// delete todoitem
+router.post('/', (req, res, next) => {
+  res.send([]);
+});
+
 module.exports = router;
+
+
+
+/* create 4 routes
+1. one route will send array of todoitems
+2. one route will create todoitem
+3. one route will edit todoitem by id
+4. route will delete todoitem by id
+also
+create a mock data array of todoItems on the server side
+and work with him */

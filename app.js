333de45622e0e2e999efb74fadbf2633e8ab0944
Vakhtang Elisabedashvili <vaxo_nba@yahoo.com>
2020-04-29 17:32:50 +0400

    deleted some files

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ client.connect(err => {
 });
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -33,7 +32,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
